b4697b7a2a0317ffe6b708c2a5400afa4e769b0f
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-14 23:11:03 -0800

    Replace jQuery template with Handlebars

--- scripts/app.js
+++ scripts/app.js
@@ -12,18 +12,10 @@ function Project(input) {
 }
 
 Project.prototype.toHtml = function () {
-  var $newProjectArticle = $('article.template').clone();
-  $newProjectArticle.removeClass('template');
-  // Fill template with data from the Project object
-  $newProjectArticle.find('h1').html(this.name);
-  $newProjectArticle.find('p').filter('.project-brief').html(this.brief);
-  var $deployLink = $newProjectArticle.find('a').filter('.deployed-url');
-  $deployLink.attr('href', this.deployment).text(this.deployment);
-  $newProjectArticle.find('img').attr('src', this.thumbnail);
-  var $repoLink = $newProjectArticle.find('a').filter('.project-repo');
-  $repoLink.attr('href', this.repo).text(this.repo);
-  $newProjectArticle.find('section').append(this.description + '<hr>');
-  return $newProjectArticle;
+  var source = $('#project-handlebars').html();
+  console.log(source);
+  var templateCompiler = Handlebars.compile(source);
+  return templateCompiler(this);
 };
 
 rawProjectData.forEach(function(rawObject) { // Pass all raw data object literals to constructor, and add the resulting object article to the projects array
