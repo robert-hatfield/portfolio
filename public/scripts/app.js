9d47bdda393f3d5718330db14fcbe14cc4fb99cc
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-21 10:48:50 -0800

    Move Project constructor object into an IFFE
    I've also added comments next to each method to ensure I remember what
     each one is doing.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,43 @@
 'use strict';
 
-function Project(input) {
-  this.name = input.name;
-  this.brief = input.brief;
-  this.description = input.description;
-  this.deployment = input.deployment;
-  this.repo = input.repo;
-  this.thumbnail = input.thumbnail
-}
+(function(module) {
+  function Project(input) { // define Project constructor
+    this.name = input.name;
+    this.brief = input.brief;
+    this.description = input.description;
+    this.deployment = input.deployment;
+    this.repo = input.repo;
+    this.thumbnail = input.thumbnail
+  }
 
-Project.all = [];
+  Project.all = []; // set an array to which to store constructed projects
 
-Project.prototype.toHtml = function () {
-  let source = $('#project-handlebars').html();
-  let templateCompiler = Handlebars.compile(source);
-  return templateCompiler(this);
-};
+  Project.prototype.toHtml = function () { // create a method to generate HTML elements for projects, using the Handlebars template
+    let source = $('#project-handlebars').html();
+    let templateCompiler = Handlebars.compile(source);
+    return templateCompiler(this);
+  };
 
-Project.loadAll = function (rawProjectArray) {
-  rawProjectArray.forEach(function(rawProjectObject) {
-    Project.all.push(new Project(rawProjectObject));
-  })
-}
-
-Project.fetchAll = function() {
-  if (localStorage.rawProjectData) {
-    console.log('Fetching from localStorage...');
-    Project.loadAll(JSON.parse(localStorage.rawProjectData)) // load from localStorage if present
-    portfolioView.initPage();
-  } else {
-    console.log('No projects in localStorage; fetching from JSON data...');
-    $.getJSON('/data/portfolio.json')
-      .then(function(data) { // use .then method to wait until .getJSON completes before running this function
-      Project.loadAll(data);
-      localStorage.rawProjectData = JSON.stringify(data); // cache to local storage once loaded
-      portfolioView.initPage();
+  Project.loadAll = function (rawProjectArray) { // Pass the project object literals from fetchAll into the constructor
+    rawProjectArray.forEach(function(rawProjectObject) {
+      Project.all.push(new Project(rawProjectObject)); // TODO: Refactor this to use the .map method
     })
   }
-}
+
+  Project.fetchAll = function() { // Get project object literals from localStorage, or the JSON file
+    if (localStorage.rawProjectData) {
+      console.log('Fetching from localStorage...');
+      Project.loadAll(JSON.parse(localStorage.rawProjectData)) // load from localStorage if present
+      portfolioView.initPage();
+    } else {
+      console.log('No projects in localStorage; fetching from JSON data...');
+      $.getJSON('/data/portfolio.json')
+        .then(function(data) { // use .then method to wait until .getJSON completes before running this function
+        Project.loadAll(data);
+        localStorage.rawProjectData = JSON.stringify(data); // cache to local storage once loaded
+        portfolioView.initPage();
+      })
+    }
+  }
+  module.Project = Project; // attach Project to the global scope so it (and its methods) are accessible outside this IFFE
+})(window);
