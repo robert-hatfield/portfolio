1754b2e3ec0acb23e5256eb190a0c32c4a0e8ea9
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-21 10:58:40 -0800

    Wrap portfolioView in an IFFE

--- public/scripts/portfolio-view.js
+++ public/scripts/portfolio-view.js
@@ -1,20 +1,24 @@
 'use strict';
 
-let portfolioView = {};
-let currentTab = 1;
+(function (module) {
+  let portfolioView = {};
+  let currentTab = 1;
 
-portfolioView.handleMainNav = function () {
-  $('.main-nav').on('click', '.tab', function(e) {
-    e.preventDefault();
-    $('section').filter('.main-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  })
-  $('.main-nav .tab:first').click();
-}
+  portfolioView.handleMainNav = function () {
+    $('.main-nav').on('click', '.tab', function(e) {
+      e.preventDefault();
+      $('section').filter('.main-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    })
+    $('.main-nav .tab:first').click();
+  }
 
-portfolioView.initPage = function() {
-  Project.all.forEach(function(htmlObject) {
-    $('#projects').append(htmlObject.toHtml());
-  });
-  portfolioView.handleMainNav();
-}
+  portfolioView.initPage = function() {
+    Project.all.forEach(function(htmlObject) {
+      $('#projects').append(htmlObject.toHtml());
+    });
+    portfolioView.handleMainNav();
+  }
+
+module.portfolioView = portfolioView;
+})(window);
