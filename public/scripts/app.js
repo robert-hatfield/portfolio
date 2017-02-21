ceb8f1cfa83faaf902d393bec981730ecc30a733
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-21 11:24:20 -0800

    Refactor the .loadAll method to use .map
    No longer using a .push inside a .forEach loop.
    I've also changed this into an ES6 arrow function, and moved the array
     declaration until just before it is used.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,19 +10,15 @@
     this.thumbnail = input.thumbnail
   }
 
-  Project.all = []; // set an array to which to store constructed projects
-
   Project.prototype.toHtml = function () { // create a method to generate HTML elements for projects, using the Handlebars template
     let source = $('#project-handlebars').html();
     let templateCompiler = Handlebars.compile(source);
     return templateCompiler(this);
   };
 
-  Project.loadAll = function (rawProjectArray) { // Pass the project object literals from fetchAll into the constructor
-    rawProjectArray.forEach(function(rawProjectObject) {
-      Project.all.push(new Project(rawProjectObject)); // TODO: Refactor this to use the .map method
-    })
-  }
+  Project.all = []; // set an array to which to store constructed projects
+  // Load all fetched JSON object literals to create an array of all portfolio projects
+  Project.loadAll = rawProjectArray => {Project.all = rawProjectArray.map(rawProjectObject => new Project(rawProjectObject))};
 
   Project.fetchAll = function() { // Get project object literals from localStorage, or the JSON file
     if (localStorage.rawProjectData) {
