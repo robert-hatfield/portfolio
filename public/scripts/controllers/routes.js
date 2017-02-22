3ccba9a0e1819cb92f5ed86598daa5a3a84a399d
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-22 10:05:46 -0800

    Add page.js library and client-routing framework

---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+page('/', indexController.init);
+page('/portfolio', portfolioController.init);
+page('/blog', soonController.init);
+page('/contact', soonController.init);
+
+page();
