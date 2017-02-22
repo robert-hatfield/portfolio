9d36b514fe9025b4ac80e2fb2dceb443d594ad15
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-22 10:44:22 -0800

    Add controllers for index & portfolio views
    This also removes the click handler for the main nav section, since it
     is no longer required with a single page application design.

--- public/scripts/controllers/portfolioController.js
+++ public/scripts/controllers/portfolioController.js
@@ -1,1 +1,10 @@
 'use strict';
+
+(function(module) {
+  const portfolioController = {};
+  portfolioController.init = () => {
+    $('main > section').hide();
+    $('#projects').show();
+  }
+  module.portfolioController = portfolioController;
+})(window);
